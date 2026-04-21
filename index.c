e8748c34d0e4ee5041536b856e4b8a27239dfeff
Balkrishna Chaudhary <balkrishnac966@gmail.com>
2026-04-21 19:54:42 +0545

    Index- Row and column

---
+++ index.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+   int marks [2][3]= {
+       { 97, 88, 99},
+       { 91, 99, 20}
+   };
+   //int marksOLD [2]= {20, 20, 10};
+   printf("%d", marks[1][3]);
+
+    return 0;
+}
