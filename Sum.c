8c592a48c536b76f53959624193563bf71c2cd7c
Balkrishna Chaudhary <balkrishnac966@gmail.com>
2026-04-23 12:24:15 +0545

    Sum of two variables

---
+++ Sum.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+   int a ;
+   int b ;
+   
+   printf("Enter the first number:\n");
+   scanf("%d",&a);
+   printf("Enter the second number:\n");
+   scanf("%d",&b);
+   int c = a+b;
+   printf("Sum of %d and %d is %d",a,b,c);
+   
+
+    return 0;
+}
